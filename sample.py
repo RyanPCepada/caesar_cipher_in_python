e42becb0455e9ca6dc91ac1639747f712aa595a0
RyanPCepada <ryancepada1992@gmail.com>
2024-04-11 12:42:55 +0800

    changing and enhancing the code

--- sample.py
+++ sample.py
@@ -1,30 +1,56 @@
-def caesar_cipher_encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shifted = ord(char) + shift
-            if char.islower():
-                if shifted > ord('z'):
-                    shifted -= 26
-                elif shifted < ord('a'):
-                    shifted += 26
-            elif char.isupper():
-                if shifted > ord('Z'):
-                    shifted -= 26
-                elif shifted < ord('A'):
-                    shifted += 26
-            encrypted_text += chr(shifted)
-        else:
-            encrypted_text += char
-    return encrypted_text
+letters = 'abcdefghijklmnopqrstuvwxyz'
 
-def caesar_cipher_decrypt(text, shift):
-    return caesar_cipher_encrypt(text, -shift)
+def encrypt(plaintext, key):
+    ciphertext = ''
+    for letter in plaintext:
+        letter = letter.lower()
+        if not letter == ' ' :
+            index = letters.find(letter)
+            if index == -1:
+                ciphertext += letter
+            else:
+                new_index = (index + key) % 26  
+                ciphertext += letters[new_index]
+    return ciphertext
 
-# Example usage:
-plaintext = "Hello, world!"
-shift = 3
-encrypted_text = caesar_cipher_encrypt(plaintext, shift)
-print("Encrypted:", encrypted_text)
-decrypted_text = caesar_cipher_decrypt(encrypted_text, shift)
-print("Decrypted:", decrypted_text)
+def decrypt(ciphertext, key):
+    plaintext = ''
+    for letter in ciphertext:
+        letter = letter.lower()
+        if not letter == ' ' :
+            index = letters.find(letter)
+            if index == -1:
+                plaintext += letter
+            else:
+                new_index = (index - key) % 26  
+                plaintext += letters[new_index]
+    return plaintext
+
+while True:
+    print()
+    print('*** CAESAR CIPHER PROGRAM ***')
+    print()
+
+    print('Do you want to encrypt or decrypt? (Type "e" for encrypt, "d" for decrypt, or "exit" to quit)')
+    user_input = input('e/d/exit: ').lower()
+    print()
+
+    if user_input == 'exit':
+        print('Exiting the program...')
+        break
+    elif user_input == 'e':
+        print('ENCRYPTION MODE SELECTED')
+        print()
+        key = int(input('Enter the key (1 through 26): '))
+        text = input('Enter the text to encrypt: ')
+        ciphertext = encrypt(text, key)
+        print(f'CIPHERTEXT: {ciphertext}')
+    elif user_input == 'd':
+        print('DECRYPTION MODE SELECTED')
+        print()
+        key = int(input('Enter the key (1 through 26): '))
+        text = input('Enter the text to decrypt: ')
+        plaintext = decrypt(text, key)
+        print(f'PLAINTEXT: {plaintext}')
+    else:
+        print('Invalid input. Please type "e" for encrypt, "d" for decrypt, or "exit" to quit.')
